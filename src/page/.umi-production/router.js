3727cf43c138f3062a255b3bcb1d5ba9b63f90eb
zhl <0278517@dahantc.com>
2018-10-01 16:50:20 +0800

    init

---
+++ src/page/.umi-production/router.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Router as DefaultRouter, Route, Switch } from 'react-router-dom';
+import dynamic from 'umi/dynamic';
+import renderRoutes from 'umi/_renderRoutes';
+
+
+let Router = DefaultRouter;
+
+let routes = [
+  {
+    "path": "/HelloWorld",
+    "exact": true,
+    "component": require('../HelloWorld.js').default
+  }
+];
+window.g_plugins.applyForEach('patchRoutes', { initialValue: routes });
+
+export default function() {
+  return (
+<Router history={window.g_history}>
+      { renderRoutes(routes, {}) }
+    </Router>
+  );
+}
